6f6b8a0debca237393ca1626080d176311afe602
Scott Maxwell <scottwillardmaxwell@protonmail.com>
2022-02-21 14:02:48 -0700

    Removed SECRET_KEY

--- seshnet/settings.py
+++ seshnet/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u)x+_y+%rena@=fn$84mt(@q)0#41omi&#(8$3puhx8$-zoy$1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
